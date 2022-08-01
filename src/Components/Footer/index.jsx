4e17992685a26ffc145b6f014192b7675838acd7
hollowblind095 <hollowblind095@gmail.com>
2022-08-01 19:29:29 -0300

    setup react & tailwind

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-sky-500 py-24">Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="bg-green-500 py-24">NavBar</div>
+  )
+}
+
+export default NavBar
